090078496ec3fa0d1b328e6597a0bbfb6d67c8cd
Gor Chalyan <gor.chalyan.2004@gmail.com>
2025-09-18 16:11:02 +0400

    Add Ex 1-13

--- homework1.c
+++ homework1.c
@@ -1,29 +1,45 @@
 #include <stdio.h>
 #include <string.h>
 int main() {
+// Ex 1 - 13
     char line[1000];
     while(fgets(line, sizeof(line), stdin) != NULL)
     {
-        int n = strlen(line);
-        line[n - 1] = '\0';
-        n--;
-        for(int i = 0, j = n - 1; i < j; i++, j--)
+        int n = strlen(line) - 1;
+        printf("Len: %d ", n);
+        for(int j = 0; j < n; j++)
         {
-            char k = line[i];
-            line[i] = line[j];
-            line[j] = k;
+            printf("*");
         }
-        if(strlen(line) > 0)
-        {    
-            printf("%s\n", line);
-        }
+        printf("\n");
     }
+
+// Ex 1 - 17
     // char line[1000];
     // while(fgets(line, sizeof(line), stdin) != NULL)
     // {
     //     if(strlen(line) > 80)
     //     {
     //         printf("%s", line);
+    //     }
+    // }
+
+// Ex 1 - 19
+    // char line[1000];
+    // while(fgets(line, sizeof(line), stdin) != NULL)
+    // {
+    //     int n = strlen(line);
+    //     line[n - 1] = '\0';
+    //     n--;
+    //     for(int i = 0, j = n - 1; i < j; i++, j--)
+    //     {
+    //         char k = line[i];
+    //         line[i] = line[j];
+    //         line[j] = k;
+    //     }
+    //     if(strlen(line) > 0)
+    //     {    
+    //         printf("%s\n", line);
     //     }
     // }
     return 0;
